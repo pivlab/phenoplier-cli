0edbbc02e636860cfed7ae1e6548abbdc479cfcc
Haoyu Zhang <haoyu_z@outlook.com>
2024-04-18 11:19:53 -0600

    [cli/run]: Clean up validation logic

--- src/phenoplier/commands/run.py
+++ src/phenoplier/commands/run.py
@@ -1,39 +1,38 @@
 import typer
 import os
-from typing import Optional, List
+from typing import Optional, Annotated, List
 
 app = typer.Typer()
 
 @app.command()
 def gls(
-    input_file: str = typer.Option(..., "--input-file", "-i", help="Path to the input file"),
-    output_file: str = typer.Option(..., "--output-file", "-o", help="Path to the output file"),
-    debug_use_ols: bool = typer.Option(None, "--debug-use-ols", help="Use OLS instead of GLS for debugging"),
-    gene_corr_file: str = typer.Option(None, "--gene-corr-file", help="Path to the gene correlation file"),
-    use_covars: Optional[str] = typer.Option(None, "--covars", help="Covariates to use"),
-    cohort_name: Optional[str] = typer.Option(None, "--cohort-name", help="Cohort name"),
-    batch_id: Optional[str] = typer.Option(None, "--batch-id", help="Batch ID"),
-    batch_n_splits: Optional[int] = typer.Option(None, "--batch-n-splits", help="Number of splits in the batch"),
-    lv_list: Optional[List[str]] = typer.Option(None, "--lv-list", help="List of latent variables"),
-    debug_use_sub_corr: bool = typer.Option(None, "--debug-use-sub-gene-corr", help="Use sub gene correlation for debugging")
+    input_file: Annotated[str, typer.Option("--input-file", "-i", help="Path to the input file")],
+    output_file: Annotated[str, typer.Option("--output-file", "-o", help="Path to the output file")],
+    phenoplier_root_dir: Annotated[str, typer.Option("--phenoplier-root-dir", envvar="PHENOPLIER_ROOT_DIR", help="Phenoplier root directory")],
+    phenoplier_metaxcan_base_dir: Annotated[str, typer.Option("--phenoplier-metaxcan-base-dir", envvar="PHENOPLIER_METAXCAN_BASE_DIR", help="Phenoplier MetaXcan base directory")],
+    debug_use_sub_corr: Annotated[bool, typer.Option("--debug-use-sub-gene-corr", help="Use sub gene correlation for debugging")] = False,
+    debug_use_ols: Annotated[bool, typer.Option("--debug-use-ols", help="Use OLS instead of GLS for debugging")] = False,
+    gene_corr_file: Annotated[str, typer.Option("--gene-corr-file", help="Path to the gene correlation file")] = None,
+    use_covars: Annotated[str, typer.Option("--covars", help="Covariates to use")] = None,
+    cohort_name: Annotated[str, typer.Option("--cohort-name", help="Cohort name")] = None,
+    batch_id: Annotated[int, typer.Option("--batch-id", help="Batch ID")] = None,
+    batch_n_splits: Annotated[int, typer.Option("--batch-n-splits", help="Number of splits in the batch")] = None,
+    lv_list: Annotated[List[str], typer.Option("--lv-list", help="List of latent variables")] = None,
 ) -> None:
     """
     Run the Generalized Least Squares (GLS) model
     """
 
-    # Environmental checks
-    required_env_vars = ["PHENOPLIER_ROOT_DIR", "PHENOPLIER_METAXCAN_BASE_DIR"]
-    for var in required_env_vars:
-        if os.getenv(var) is None:
-            typer.echo(f"Error: {var} environment variable is not set", err=True)
-            raise typer.Exit(code=1)
-
     # Check if both "debug_use_ols" and "gene_corr_file" are None
-    if debug_use_ols is None and gene_corr_file is None:
-        typer.echo("Error: either --debug-use-ols or --gene-corr-file <value> must be provided", err=True)
-        raise typer.Exit(code=1)
+    if debug_use_ols is False and gene_corr_file is None:
+        # typer.echo("Error: either --debug-use-ols or --gene-corr-file <value> must be provided", err=True)
+        raise typer.BadParameter("Either --debug-use-ols or --gene-corr-file <value> must be provided")
+    # and they shold not be both provided
+    if debug_use_ols is True and gene_corr_file is not None:
+        typer.echo("Error: only one of --debug-use-ols or --gene-corr-file <value> can be provided", err=True)
+        # raise typer.BadParameter("Only one of --debug-use-ols or --gene-corr-file <value> can be provided")
 
-    # Batch processing argument validation
+    # Validatae batch processing argument
     if (batch_id is None) != (batch_n_splits is None):
         typer.echo("Error: both --batch-id and --batch-n-splits must be provided together", err=True)
         raise typer.Exit(code=1)
@@ -46,6 +45,7 @@ def gls(
     covars_args = f"--covars {use_covars}" if use_covars else ""
     cohort_args = ""
     if cohort_name:
+        # FIXME: hardcoded
         cohort_metadata_dir = f"{os.getenv('PHENOPLIER_RESULTS_GLS')}/gene_corrs/cohorts/{cohort_name}/gtex_v8/mashr/"
         cohort_args = f"--cohort-metadata-dir {cohort_metadata_dir}"
 
