beac2b33854e0d184b9701ff9b493975491527eb
Haoyu Zhang <haoyu_z@outlook.com>
2024-04-24 15:40:21 -0600

    [test]: Integrate Phenoplier's original tests

--- scripts/dev/setup_dev_env.sh
+++ scripts/dev/setup_dev_env.sh
@@ -11,5 +11,8 @@ export PHENOPLIER_CODE_DIR="./"
 # Append the PhenoPLIER code lib to the PYTHONPATH
 export PYTHONPATH="${PHENOPLIER_CODE_DIR}/libs:${PYTHONPATH}"
 
+# Set test folder
+export PHENOPLIER_TEST_DIR="${PHENOPLIER_CODE_DIR}/test"
+
 # Load the PhenoPLIER configuration
 eval `python libs/conf.py`
