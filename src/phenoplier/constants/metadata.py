207e4146f05b3b5d9522656cd7a1cacc139ab5cb
Haoyu Zhang <haoyu_z@outlook.com>
2024-04-29 12:02:21 -0600

    [cli/init]: Add a template init command

--- src/phenoplier/constants/metadata.py
+++ src/phenoplier/constants/metadata.py
@@ -3,3 +3,5 @@ from importlib.metadata import distribution
 dist = distribution("phenoplier")
 APP_NAME = dist.metadata["name"]
 APP_VERSION = dist.version
+
+CONFIG_FILE = APP_NAME + "_config.toml"
