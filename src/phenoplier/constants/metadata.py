1379ffbc271ab0b9b7cc736228613357a3f1cac2
Haoyu Zhang <haoyu_z@outlook.com>
2024-04-29 17:21:44 -0600

    [cli/init]: Output default user config

--- src/phenoplier/constants/metadata.py
+++ src/phenoplier/constants/metadata.py
@@ -1,7 +1,12 @@
 from importlib.metadata import distribution
+from pathlib import Path
 
 dist = distribution("phenoplier")
 APP_NAME = dist.metadata["name"]
 APP_VERSION = dist.version
+APP_CODE_DIR = Path(__file__).parent.parent.parent.parent.resolve()
+APP_TEST_DIR = APP_CODE_DIR / "test"
 
-CONFIG_FILE = APP_NAME + "_config.toml"
+CONFIG_FOLDER = Path.home() / ("." + APP_NAME)
+CONFIG_FILE = CONFIG_FOLDER / "config.toml"
+USER_SETTINGS_FILE = CONFIG_FOLDER / "user_settings.toml"
