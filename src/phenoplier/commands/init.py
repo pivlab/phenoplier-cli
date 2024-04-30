1fbc8aabb99cc3fe3a926ce20e11d6414ee8a3cd
Haoyu Zhang <haoyu_z@outlook.com>
2024-04-29 23:00:38 -0600

    [cli/run]: Load user settings from file and execute

--- src/phenoplier/commands/init.py
+++ src/phenoplier/commands/init.py
@@ -1,7 +1,7 @@
 """
 This file implements the "init" command for the Phenoplier CLI
 
-The "init" command initializes the Phenoplier project by creating the necessary configuration files
+The "init" command initializes the Phenoplier project by creating the necessary settingsuration files
 at the user's home directory.
 """
 from typing import Annotated
@@ -14,44 +14,44 @@ from phenoplier.constants.metadata import CONFIG_FILE, USER_SETTINGS_FILE
 from phenoplier.constants.templates import USER_SETTINGS
 
 
-def create_user_config():
-    config_file = CONFIG_FILE
-    if not CONFIG_FILE.exists():
-        config = USER_SETTINGS
-        config_file.parent.mkdir(parents=True, exist_ok=True)
-        config_file.write_text(tomlkit.dumps(config))
-        typer.echo("Config file created at " + str(config_file) + ".")
+def create_user_settings():
+    settings_file = USER_SETTINGS_FILE
+    if not settings_file.exists():
+        settings = USER_SETTINGS
+        settings_file.parent.mkdir(parents=True, exist_ok=True)
+        settings_file.write_text(tomlkit.dumps(settings))
+        typer.echo("Config file created at " + str(settings_file) + ".")
     else:
-        typer.echo("Config file already exists at " + str(config_file) + ".")
+        typer.echo("Config file already exists at " + str(settings_file) + ".")
 
 
 def init_required(function):
     @wraps(function)
     def wrapper(*args, **kwargs):
-        config_file = CONFIG_FILE
-        if not config_file.exists():
+        settings_file = CONFIG_FILE
+        if not settings_file.exists():
             typer.echo("Config file does not exist. Please run the init command first.")
             raise typer.Exit()
-        # Optionally, read config here if needed for the function
-        with open(config_file, "r") as f:
-            config = tomlkit.loads(f.read())  # Using tomlkit.loads to read TOML data from file
+        # Optionally, read settings here if needed for the function
+        with open(settings_file, "r") as f:
+            settings = tomlkit.loads(f.read())  # Using tomlkit.loads to read TOML data from file
         return function(*args, **kwargs)
 
     return wrapper
 
-# TODO: Add a prompt to ask the user if they want to overwrite the existing config file
+# TODO: Add a prompt to ask the user if they want to overwrite the existing settings file
 def init(
     output_file: Annotated[str, typer.Option("--output-file", "-o", help="Path to the output user settings file")] = str(USER_SETTINGS_FILE),
 ):
     """
-    Initialize the user config file in the home directory in TOML format.
+    Initialize the user settings file in the home directory in TOML format.
     """
-    create_user_config()
+    create_user_settings()
 
 
 @init_required
 def hello():
     """
-    Example command that requires a config file.
+    Example command that requires a settings file.
     """
-    typer.echo("Hello, the config file exists!")
+    typer.echo("Hello, the settings file exists!")
--- src/phenoplier/constants/metadata.py
+++ src/phenoplier/constants/metadata.py
@@ -1,12 +1,17 @@
+"""This module provides the metadata for Phenoplier."""
+
 from importlib.metadata import distribution
 from pathlib import Path
+import tempfile
 
+# Package metadata
 dist = distribution("phenoplier")
 APP_NAME = dist.metadata["name"]
 APP_VERSION = dist.version
 APP_CODE_DIR = Path(__file__).parent.parent.parent.parent.resolve()
 APP_TEST_DIR = APP_CODE_DIR / "test"
 
+# Config files
 CONFIG_FOLDER = Path.home() / ("." + APP_NAME)
 CONFIG_FILE = CONFIG_FOLDER / "config.toml"
 USER_SETTINGS_FILE = CONFIG_FOLDER / "user_settings.toml"
