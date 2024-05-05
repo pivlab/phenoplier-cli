6620ef43aa063cc7f28f7a5764ca42d1781972fa
Haoyu Zhang <haoyu_z@outlook.com>
2024-05-04 22:41:41 -0600

    [refactor]: Remove "commands" package
    Putting all logic just inside cli.py is cleaner and clearer. No need to complex the package structure

---
+++ phenoplier/templates/user_settings.py
@@ -0,0 +1,11 @@
+from phenoplier.constants.metadata import APP_CODE_DIR, APP_TEST_DIR
+from pathlib import Path
+import tempfile
+
+DEFAULT = {
+    "ROOT_DIR":         str(Path(tempfile.gettempdir(), "phenoplier").resolve()),
+    "MANUSCRIPT_DIR":   str(Path(tempfile.gettempdir(), "manuscript").resolve()),
+    "GTEX_V8_DIR":      "",
+    # "N_JOBS":           None,
+    # "N_JOBS_HIGH":      None,
+    }
