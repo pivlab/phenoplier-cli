1e15ae049ac398e78dea805da6e1099401c2f636
Haoyu Zhang <haoyu_z@outlook.com>
2024-05-05 22:24:41 -0600

    [core/conf]: Adopt Dynaconf for configuration management

--- phenoplier/__main__.py
+++ phenoplier/__main__.py
@@ -1,10 +1,10 @@
 """Entry point for Phenoplier CLI."""
-import phenoplier.cli as cli
-from phenoplier.config import APP_NAME
+from . import cli
+from .config import settings
 
 
 def main():
-    cli.app(prog_name=APP_NAME)
+    cli.app(prog_name=settings.APP_NAME)
 
 
 if __name__ == "__main__":
--- phenoplier/config.py
+++ phenoplier/config.py
@@ -1,16 +1,40 @@
-"""This module provides the metadata for Phenoplier."""
-
-from importlib.metadata import distribution
+from dynaconf import Dynaconf
+import tempfile
 from pathlib import Path
+from importlib.metadata import distribution
 
-# Package metadata
-dist = distribution("phenoplier")
-APP_NAME = dist.metadata["name"]
-APP_VERSION = dist.version
-APP_CODE_DIR = Path(__file__).parent.resolve()
-APP_TEST_DIR = APP_CODE_DIR / "test"
+_APP_NAME = "phenoplier"
+dist = distribution(_APP_NAME)
+_APP_VERSION = dist.version
 
 # Config files
-CONFIG_FOLDER = Path.home() / ("." + APP_NAME)
+CONFIG_FOLDER = Path.home() / ("." + _APP_NAME)
 CONFIG_FILE = CONFIG_FOLDER / "config.toml"
 USER_SETTINGS_FILE = CONFIG_FOLDER / "user_settings.toml"
+
+# The environment variables name supersede these settings
+# Prefix the environment variables with `$_APP_NAME` to automatically load them
+# E.g. `export {$_APP_NAME}_FOO=bar` will load `FOO=bar` in the settings
+
+settings = Dynaconf(
+    envvar_prefix=_APP_NAME.upper(),
+    settings_files=["user_settings.toml", "internal_settings.toml"],
+
+    APP_NAME=_APP_NAME,
+    APP_VERSION=_APP_VERSION,
+
+    # Specifies the main directory where all data and results generated are stored.
+    # When setting up the environment for the first time, input data will be
+    # automatically downloaded into a subfolder of ROOT_DIR. If not specified, it
+    # defaults to the 'phenoplier' subfolder in the temporary directory of the
+    # operating system (i.e. '/tmp/phenoplier' in Unix systems).
+    #
+    ROOT_DIR=str(Path(tempfile.gettempdir(), _APP_NAME).resolve()),
+    # Directory contains the git repository
+    CODE_DIR=str(Path(__file__).resolve().parent),
+    # Directory contains the tests
+    TESTS_DIR=str(Path(__file__).resolve().parent / "test"),
+)
+
+# `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
+# `settings_files` = Load these files in the order.
