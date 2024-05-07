7829162732285bcbdf4fc4de753c55bef88dbc22
Haoyu Zhang <haoyu_z@outlook.com>
2024-05-07 16:05:44 -0600

    [test]: Fix pythonpath for testing

--- test/regress/cli/test_version.py
+++ test/regress/cli/test_version.py
@@ -1,12 +1,13 @@
 from typer.testing import CliRunner
-from phenoplier import cli
 from importlib.metadata import distribution
+from phenoplier import cli
 
 runner = CliRunner()
 # Get package metadata
 dist = distribution("phenoplier")
 app_name = dist.metadata["name"]
 app_version = dist.version
+
 
 def test_version0():
     result = runner.invoke(cli.app, ["--version"])
