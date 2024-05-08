651cfd031be1d789d8c7d945944d69640bde4e9b
Haoyu Zhang <haoyu_z@outlook.com>
2024-05-08 15:21:48 -0600

    [cli/init]: Remove unnecessary code

--- phenoplier/cli.py
+++ phenoplier/cli.py
@@ -87,32 +87,7 @@ def init(
     """
     Initialize a user settings file in the home directory in TOML format.
     """
-    import subprocess
-    from importlib import reload, import_module
-    from .data import setup_data
-
-    print(output_file)
-    settings.ROOT_DIR = output_file
-    reload(import_module(".config", package="phenoplier"))
-
-    # Define the path to the script
-    script_path = Path(settings.CODE_DIR, "data.py").resolve()
-
-    # Define the command with the script path variable
-    actions = [
-        "download_phenomexcan_rapid_gwas_pheno_info",
-        "download_phenomexcan_rapid_gwas_data_dict_file",
-        "download_uk_biobank_coding_3",
-        "download_uk_biobank_coding_6",
-        "download_phenomexcan_gtex_gwas_pheno_info",
-        "download_gene_map_id_to_name",
-        "download_gene_map_name_to_id",
-        "download_biomart_genes_hg38",
-        "download_multiplier_model_z_pkl"
-    ]
-
-    setup_data(actions=actions)
-
+    raise NotImplementedError("This function is not implemented yet.")
     # def create_user_settings():
     #     settings_file = USER_SETTINGS_FILE
     #     if not settings_file.exists():
