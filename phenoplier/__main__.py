fd0c261834cd1f3319ae320824880cb9743c9777
Haoyu Zhang <haoyu_z@outlook.com>
2024-05-15 13:00:12 -0600

    [exe]: Add package entry to path

--- phenoplier/__main__.py
+++ phenoplier/__main__.py
@@ -1,10 +1,12 @@
 """Entry point for Phenoplier CLI."""
 from . import cli
 from .config import settings
+import sys
 
 
 def main():
     cli.app(prog_name=settings.APP_NAME)
+    sys.exit()
 
 
 if __name__ == "__main__":
