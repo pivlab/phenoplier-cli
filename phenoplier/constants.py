c69713a1a9960acaeac162e51a37c4f5b9001c09
Haoyu Zhang <haoyu_z@outlook.com>
2024-05-16 12:22:52 -0600

    [cli]: Move help messages to constants.py

---
+++ phenoplier/constants.py
@@ -0,0 +1,29 @@
+from types import MappingProxyType as ImmutableDict
+
+RUN_GLS = ImmutableDict(
+    {
+        "input_file": "File path to S-MultiXcan result file (tab-separated and with at least columns 'gene' and 'pvalue')",
+        "output_file": "File path where results will be written to",
+        "gene_corr_file": "Path to a gene correlations file or folder. It's is mandatory if running a GLS model, and not necessary for OLS",
+        "use_covars": "List of covariates to use",
+        "cohort_name": "Cohort name",
+        "lv_list": "List of LV (gene modules) identifiers on which an association will be computed. All the rest not in the list are ignored",
+        "debug_use_sub_corr": "Use an LV-specific submatrix of the gene correlation matrix",
+        "debug_use_ols": "Choose which regression model to use",
+        "batch_id": "Batch ID",
+        "batch_n_splits": "Number of splits in the batch"
+    }
+)
+
+RUN = ImmutableDict(
+    {
+        "gls": RUN_GLS
+    }
+)
+
+CLI = ImmutableDict(
+    {
+        "run": RUN
+    }
+)
+
