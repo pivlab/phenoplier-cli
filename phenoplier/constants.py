d2a8729d54827a0e00e15e08d9b70bba1bcf193a
Haoyu Zhang <haoyu_z@outlook.com>
2024-05-18 17:21:14 -0600

    [gls]: Improve the logic for default covars values

--- phenoplier/constants.py
+++ phenoplier/constants.py
@@ -1,11 +1,32 @@
 from types import MappingProxyType as ImmutableDict
 
-RUN_GLS = ImmutableDict(
+# List of all supported covariates
+COVAR_OPTIONS = [
+    "all",
+    "gene_size",
+    "gene_size_log",
+    "gene_density",
+    "gene_density_log",
+    "gene_n_snps_used",
+    "gene_n_snps_used_log",
+    "gene_n_snps_used_density",
+    "gene_n_snps_used_density_log",
+]
+
+RUN_GLS_DEFAULTS = ImmutableDict(
+    {
+        "covars": "gene_size gene_size_log gene_density gene_density_log"
+    }
+)
+
+RUN_GLS_ARGS = ImmutableDict(
     {
         "input_file": "File path to S-MultiXcan result file (tab-separated and with at least columns 'gene' and 'pvalue').",
         "output_file": "File path where results will be written to.",
         "gene_corr_file": "Path to a gene correlations file or folder. It's is mandatory if running a GLS model, and not necessary for OLS.",
-        "covars": "List of covariates to use. Separate them by spaces.",
+        "covars": f"List of covariates to use. Separate them by spaces. If provided with 'default', the default "
+                  f"covariates will be used. Available covariates:[{' '.join(COVAR_OPTIONS)}]. And the default "
+                  f"covariates are: [{RUN_GLS_DEFAULTS['covars']}].",
         "cohort_name": "Cohort name.",
         "lv_list": "List of LV (gene modules) identifiers on which an association will be computed. All the rest not in the list are ignored.",
         "debug_use_sub_corr": "Use an LV-specific submatrix of the gene correlation matrix.",
@@ -15,9 +36,10 @@ RUN_GLS = ImmutableDict(
     }
 )
 
+
 RUN = ImmutableDict(
     {
-        "gls": RUN_GLS
+        "gls": RUN_GLS_ARGS
     }
 )
 
