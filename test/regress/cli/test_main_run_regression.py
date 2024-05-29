877e7201ea01c88ac487c1121ab37a34f1ffa735
Haoyu Zhang <haoyu_z@outlook.com>
2024-05-29 14:07:46 -0600

    [cli]: Clean up code

--- test/regress/cli/test_main_run_regression.py
+++ test/regress/cli/test_main_run_regression.py
@@ -1,8 +1,9 @@
 import os
+import tempfile
+
 from typer.testing import CliRunner
 from pathlib import Path
 from pytest import mark
-from rich import print
 from phenoplier import cli
 from phenoplier.config import settings
 from .utils import diff_tsv, get_test_output_dir
@@ -28,24 +29,28 @@ def _test_random_pheno(idx: int, with_default_covars: bool):
     test_output_dir = get_test_output_dir(__file__)
     Path(test_output_dir).mkdir(parents=True, exist_ok=True)
     print(test_output_dir)
+    # Init project files
+    temp_project_dir = get_test_output_dir(__file__) / "temp_project"
+    temp_project_dir.mkdir(parents=True, exist_ok=True)
+    runner.invoke(cli.app, f"init -p {temp_project_dir}")
     # Prepare cli options
     input_file = Path(f"{test_dir}/data/gls/covars_test/random.pheno{idx}-gtex_v8-mashr-smultixcan.txt").resolve()
     output_file_prefix = "with_covars_" if with_default_covars else "without_covars_"
     output_file = Path(f"{test_output_dir}/{output_file_prefix}random.pheno{idx}.tsv").resolve()
     gene_corr_file = Path(f"{test_dir}/data/gls/covars_test/gene_corr_file/gene_corrs-symbols-within_distance_5mb.per_lv").resolve()
-    option = f"{_COMMAND_NAME} -i {input_file} -o {output_file} --gene-corr-file {gene_corr_file}"
+    option = f"{_COMMAND_NAME} -i {input_file} -o {output_file} --gene-corr-file {gene_corr_file} -p {temp_project_dir}"
     if with_default_covars:
         option += ' --covars default'
+    print(f"Running command: {option}")
     # Delete output file if it already exists
     if output_file.exists():
         output_file.unlink()
     # Run command
-    print(f"Running command: {option}")
     result = runner.invoke(cli.app, option)
     print(result.stdout)
     if result.exit_code != 0:
         print(result.stdout)
-        print(result.stderr)
+        # print(result.stderr)
     assert result.exit_code == 0
     # Compare output
     expected_output_file_folder = "with_covars" if with_default_covars else "without_covars"
