2ea10fa6293ef547660dade2dda4aa90b9726655
Haoyu Zhang <haoyu_z@outlook.com>
2024-05-30 13:21:38 -0600

    [cli]: Discard the .cache folder and load settings directly

--- test/unittest/test_cli.py
+++ test/unittest/test_cli.py
@@ -1,9 +1,12 @@
-from phenoplier.cli import load_settings_files, create_settings_files, remove_settings_files
-from pytest import fixture, mark, raises
-import phenoplier.config
-from pathlib import Path
 import random
-import importlib
+import shutil
+from pathlib import Path
+
+
+from pytest import mark, raises
+
+import phenoplier.config
+from phenoplier.cli import load_settings_files, create_settings_files, remove_settings_files
 
 _TEST_OUTPUT_DIR = phenoplier.config.settings.TEST_OUTPUT_DIR / "unittest"
 
@@ -27,25 +30,28 @@ def test_load_settings_files_on_non_existing_dir(directory):
     with (raises(Exception)): load_settings_files(directory)
 
 
-# def test_load_settings_files():
-#     # Create the settings files
-#     directory = Path(_TEST_OUTPUT_DIR, test_load_settings_files.__name__)
-#     remove_settings_files(directory)
-#     remove_settings_files(Path(phenoplier.config.settings.CACHE_DIR))
-#     # Before loading the settings files, the following settings should not be present
-#     unloaded_settings = ['DATA_DIR']
-#     for s in unloaded_settings:
-#         assert not hasattr(phenoplier.config.settings, s)
-#     create_settings_files(directory)
-#     print(hex(id(phenoplier.config.settings)))
-#     load_settings_files(directory)
-#     print(hex(id(phenoplier.config.settings)))
-#     # importlib.reload(phenoplier.config)
-#     # Check that previous unloaded settings files were loaded
-#     try:
-#         for s in unloaded_settings:
-#             assert hasattr(phenoplier.config.settings, s)
-#     finally:
-#         # Clean up
-#         remove_settings_files(directory)
-#         remove_settings_files(Path(phenoplier.config.settings.CACHE_DIR))
+def test_load_settings_files():
+    # Create the settings files
+    directory = Path(_TEST_OUTPUT_DIR, test_load_settings_files.__name__)
+    remove_settings_files(directory)
+    # Before loading the settings files, the following settings should not be present
+    unloaded_settings = ['_TEST_SETTINGS0', "_TEST_SETTINGS0A", "TEST2._ATTR"]
+    for s in unloaded_settings:
+        assert not hasattr(phenoplier.config.settings, s)
+    # Now create the default settings
+    create_settings_files(directory)
+    # Now add additional testing configs
+    more_settings_files = [Path("./test_load_settings0.toml"), Path("./test_load_settings1.toml")]
+    for s in more_settings_files:
+        src = Path(__file__).resolve().parent / s
+        dst = directory / s
+        print(src, dst)
+        shutil.copy2(src, dst)
+    load_settings_files(directory, more_settings_files)
+    # Check that previous unloaded settings files were loaded
+    try:
+        for s in unloaded_settings:
+            assert hasattr(phenoplier.config.settings, s)
+    finally:
+        # Clean up
+        shutil.rmtree(directory)
