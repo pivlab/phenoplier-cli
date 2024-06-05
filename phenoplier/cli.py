963642396b088207b9d8dda73763ad2ce4eae28e
Haoyu Zhang <haoyu_z@outlook.com>
2024-06-05 14:57:58 -0600

    [cli/ref]: Put util functions in a dedicated module

--- phenoplier/cli.py
+++ phenoplier/cli.py
@@ -23,6 +23,7 @@ from phenoplier.commands.run.correlation.correlate import correlate
 from phenoplier.commands.run.correlation.postprocess import postprocess
 from phenoplier.commands.run.correlation.filter import filter
 from phenoplier.commands.run.correlation.generate import generate
+from phenoplier.commands.utils import load_settings_files
 
 LOG_FORMAT = "%(levelname)s: %(message)s"
 
@@ -127,52 +128,6 @@ def create_settings_files(directory: Path) -> None:
             shutil.copy2(template_file, settings_file)
             print(f"Config file {str(file_name)} created at {directory}")
 
-
-def remove_settings_files(directory: Path) -> None:
-    for file_name in SETTINGS_FILES:
-        settings_file = Path(directory) / file_name
-        if settings_file.exists():
-            settings_file.unlink()
-            print(f"Config file {str(file_name)} removed from {directory}")
-
-
-def check_settings_files(directory: Path) -> None:
-    for file_name in SETTINGS_FILES:
-        settings_file = Path(directory) / file_name
-        if not settings_file.exists():
-            raise typer.BadParameter(f"Config file {str(file_name)} does not exist at {directory}. Please run 'phenoplier init' first.")
-
-
-def load_settings_files(directory: Path, more_files: List[Path] = []) -> None:
-    """
-    Load the settings files from the specified directory. The expected side effect is that after this function is called,
-    settings defined in the toml config files will be available as attributes of the settings object.
-    :param directory: The directory where the settings files are located.
-    :param more_files: A list of settings files other than the default ones to load. Those files should be also in
-    the same directory as the default settings file.
-    """
-
-    # Check if the directory exists
-    if not directory.exists():
-        raise typer.BadParameter(f"Provided config directory does not exist: {directory}")
-    # Check if the settings files exist in the directory
-    check_settings_files(directory)
-    # Load the default settings
-    for file_name in SETTINGS_FILES:
-        settings_file = directory / file_name
-        if settings_file.exists():
-            settings.load_file(settings_file)
-            print(f"Config file {str(file_name)} loaded from {directory}")
-        else:
-            raise typer.BadParameter(f"Config file {str(file_name)} does not exist at {directory}.")
-    # Load the additional settings
-    for curr_dir_file in more_files:
-        file = directory / curr_dir_file
-        if not file.exists():
-            raise typer.BadParameter(f"Config file {str(file)} does not exist at {directory}.")
-        settings_file = directory / file
-        settings.load_file(settings_file)
-        print(f"Additional config file {str(file)} loaded from {directory}")
 
 
 # TODO: Add a prompt to ask the user if they want to overwrite the existing settings file
