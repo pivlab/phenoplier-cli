8631cdc7185acc3b0b377b1f4b80332f411ca343
Haoyu Zhang <haoyu_z@outlook.com>
2024-06-11 15:19:55 -0600

    [test/corr]: Update test placement

--- test/regress/cli/test_main_run_corr_preprocess.py
+++ test/regress/cli/test_main_run_corr_preprocess.py
@@ -6,22 +6,6 @@ from phenoplier import cli
 
 runner = CliRunner()
 IN_GITHUB_ACTIONS = os.getenv("GITHUB_ACTIONS") == "true"
-_BASE_COMMAND_NAME = "run gene-corr"
-
-
-@mark.parametrize("options, expected_output", [
-    ([f"{_BASE_COMMAND_NAME} --help", f"{_BASE_COMMAND_NAME} -h"], ""),
-    ([f"{_BASE_COMMAND_NAME} cov --help", f"{_BASE_COMMAND_NAME} cov -h"], ""),
-    ([f"{_BASE_COMMAND_NAME} preprocess --help", f"{_BASE_COMMAND_NAME} preprocess -h"], ""),
-    ([f"{_BASE_COMMAND_NAME} correlate --help", f"{_BASE_COMMAND_NAME} correlate -h"], ""),
-    ([f"{_BASE_COMMAND_NAME} postprocess --help", f"{_BASE_COMMAND_NAME} postprocess -h"], ""),
-    ([f"{_BASE_COMMAND_NAME} generate --help", f"{_BASE_COMMAND_NAME} generate -h"], ""),
-])
-def test_options(options, expected_output):
-    for i in range(len(options)):
-        result = runner.invoke(cli.app, options[i])
-        assert result.exit_code == 0
-        assert expected_output in result.stdout
 
 
 # Define the placeholders in the command
