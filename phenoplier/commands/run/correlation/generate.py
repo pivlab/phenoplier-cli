2b8f70bc84bb7d593fd46ac7bb85d861c4c0e0c9
Haoyu Zhang <haoyu_z@outlook.com>
2024-06-12 13:26:16 -0600

    [cli/generate]: Finish functionality

--- phenoplier/commands/run/correlation/generate.py
+++ phenoplier/commands/run/correlation/generate.py
@@ -14,6 +14,47 @@ from phenoplier.commands.enums import Cohort, RefPanel, EqtlModel
 from phenoplier.commands.utils import load_settings_files
 
 
+def exists_df(output_dir, base_filename):
+    full_filepath = output_dir / (base_filename + ".npz")
+    return full_filepath.exists()
+
+
+def store_df(output_dir, nparray, base_filename):
+    if base_filename in ("metadata", "gene_names"):
+        np.savez_compressed(output_dir / (base_filename + ".npz"), data=nparray)
+    else:
+        sparse.save_npz(output_dir / (base_filename + ".npz"), sparse.csc_matrix(nparray), compressed=False)
+
+
+def get_output_dir(gene_corr_filename, OUTPUT_DIR_BASE):
+    path = OUTPUT_DIR_BASE / gene_corr_filename
+    assert path.exists()
+    return path.with_suffix(".per_lv")
+
+
+def compute_chol_inv(lv_code, gene_corrs_dict, multiplier_z, OUTPUT_DIR_BASE, reference_panel, eqtl_model, lv_percentile):
+    for gene_corr_filename, gene_corrs in gene_corrs_dict.items():
+        output_dir = get_output_dir(gene_corr_filename, OUTPUT_DIR_BASE)
+        output_dir.mkdir(parents=True, exist_ok=True)
+
+        lv_data = multiplier_z[lv_code]
+        corr_mat_sub = GLSPhenoplier.get_sub_mat(gene_corrs, lv_data, lv_percentile)
+        store_df(output_dir, corr_mat_sub.to_numpy(), f"{lv_code}_corr_mat")
+
+        chol_mat = np.linalg.cholesky(corr_mat_sub)
+        chol_inv = np.linalg.inv(chol_mat)
+        store_df(output_dir, chol_inv, lv_code)
+
+        if not exists_df(output_dir, "metadata"):
+            metadata = np.array([reference_panel, eqtl_model])
+            store_df(output_dir, metadata, "metadata")
+
+        if not exists_df(output_dir, "gene_names"):
+            gene_names = np.array(gene_corrs.index.tolist())
+            store_df(output_dir, gene_names, "gene_names")
+
+
+
 def generate(
     cohort_name:        Annotated[Cohort, typer.Option("--cohort-name", "-c", help="Cohort name")],
     reference_panel:    Annotated[RefPanel, typer.Option("--reference-panel", "-r", help="Reference panel such as 1000G or GTEX_V8")],
@@ -25,63 +66,26 @@ def generate(
     """
     Computes an LV-specific correlation matrix by using the top genes in that LV only.
     """
-    def validate_inputs(cohort_name, reference_panel, eqtl_model, lv_code, lv_percentile):
-        assert cohort_name is not None and len(cohort_name) > 0, "A cohort name must be given"
-        assert reference_panel is not None and len(reference_panel) > 0, "A reference panel must be given"
-        assert eqtl_model is not None and len(eqtl_model) > 0, "A prediction/eQTL model must be given"
-        assert lv_code is not None and len(lv_code) > 0, "An LV code must be given"
-        if lv_percentile is not None:
-            lv_percentile = float(lv_percentile)
-        return cohort_name.lower(), reference_panel.lower(), eqtl_model.lower(), lv_code, lv_percentile
 
-    cohort_name, reference_panel, eqtl_model, lv_code, lv_percentile = validate_inputs(cohort_name, reference_panel, eqtl_model, lv_code, lv_percentile)
-
+    eqtl_model = eqtl_model.lower()
+    reference_panel = reference_panel.lower()
+    lv_code = "LV" + str(lv_code)
     load_settings_files(project_dir)
 
-    OUTPUT_DIR_BASE = conf.RESULTS["GLS"] / "gene_corrs" / "cohorts" / cohort_name / reference_panel / eqtl_model
+    OUTPUT_DIR_BASE = Path(conf.RESULTS["GLS"]) / "gene_corrs" / "cohorts" / cohort_name / reference_panel / eqtl_model
     gene_corrs_dict = {f.name: pd.read_pickle(f) for f in OUTPUT_DIR_BASE.glob("gene_corrs-symbols*.pkl")}
-    multiplier_z = pd.read_pickle(conf.MULTIPLIER["MODEL_Z_MATRIX_FILE"])
-
-    def exists_df(output_dir, base_filename):
-        full_filepath = output_dir / (base_filename + ".npz")
-        return full_filepath.exists()
-
-    def store_df(output_dir, nparray, base_filename):
-        if base_filename in ("metadata", "gene_names"):
-            np.savez_compressed(output_dir / (base_filename + ".npz"), data=nparray)
-        else:
-            sparse.save_npz(output_dir / (base_filename + ".npz"), sparse.csc_matrix(nparray), compressed=False)
-
-    def get_output_dir(gene_corr_filename):
-        path = OUTPUT_DIR_BASE / gene_corr_filename
-        assert path.exists()
-        return path.with_suffix(".per_lv")
-
-    def compute_chol_inv(lv_code):
-        for gene_corr_filename, gene_corrs in gene_corrs_dict.items():
-            output_dir = get_output_dir(gene_corr_filename)
-            output_dir.mkdir(parents=True, exist_ok=True)
-
-            lv_data = multiplier_z[lv_code]
-            corr_mat_sub = GLSPhenoplier.get_sub_mat(gene_corrs, lv_data, lv_percentile)
-            store_df(output_dir, corr_mat_sub.to_numpy(), f"{lv_code}_corr_mat")
-
-            chol_mat = np.linalg.cholesky(corr_mat_sub)
-            chol_inv = np.linalg.inv(chol_mat)
-            store_df(output_dir, chol_inv, lv_code)
-
-            if not exists_df(output_dir, "metadata"):
-                metadata = np.array([reference_panel, eqtl_model])
-                store_df(output_dir, metadata, "metadata")
-
-            if not exists_df(output_dir, "gene_names"):
-                gene_names = np.array(gene_corrs.index.tolist())
-                store_df(output_dir, gene_names, "gene_names")
+    multiplier_z = pd.read_pickle(conf.GENE_MODULE_MODEL["MODEL_Z_MATRIX_FILE"])
 
     lvs_chunks = [[lv_code]]
 
     with ProcessPoolExecutor(max_workers=1) as executor, tqdm(total=len(lvs_chunks), ncols=100) as pbar:
-        tasks = [executor.submit(compute_chol_inv, chunk) for chunk in lvs_chunks]
+        tasks = [
+            executor.submit(compute_chol_inv, chunk[0], gene_corrs_dict, multiplier_z, OUTPUT_DIR_BASE, reference_panel, eqtl_model, lv_percentile)
+            for chunk in lvs_chunks
+        ]
         for future in as_completed(tasks):
             res = future.result()
             pbar.update(1)
+
+    print(f"Computation for {lv_code} done. Output to {OUTPUT_DIR_BASE}")
+
