0aafe055adf67b367fd5433635afa48252951201
Haoyu Zhang <haoyu_z@outlook.com>
2024-07-21 11:42:14 -0600

    [test/gls]: Finish testing input p-value data column

--- phenoplier/constants/message.py
+++ phenoplier/constants/message.py
@@ -3,7 +3,7 @@ from enum import StrEnum
 
 class RegressionError(StrEnum):
     NO_GENE_NAME_COLUMN = "Mandatory columns not present in data 'gene_name'"
-
+    NO_P_VALUE_COLUMN = "Mandatory columns not present in data 'p_value'"
 
 class RegressionInfo(StrEnum):
     LOADING_INPUT = "Loading input data..."
