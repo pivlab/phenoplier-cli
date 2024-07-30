8c931f5a08c6556c01612a5fd0816cef9291c61f
Haoyu Zhang <haoyu_z@outlook.com>
2024-07-29 22:00:48 -0600

    [cli/corr]: Add "correlate" to the pipeline

---
+++ phenoplier/commands/invoker.py
@@ -0,0 +1,197 @@
+"""
+This module contains encapsulated function calls to invoke individual cli commands. Useful for pipeline automation
+and testing.
+"""
+from pathlib import Path
+from typing import Tuple, List
+
+from typer.testing import CliRunner
+
+from phenoplier import cli
+from phenoplier.config import settings as conf
+from phenoplier.commands.util.enums import Cohort, RefPanel, EqtlModel
+
+runner = CliRunner()
+
+
+# @formatter:off
+def invoke_corr_preprocess(
+        cohort:                     Cohort,
+        gwas_file:                  Path,
+        spredixcan_folder:          Path,
+        spredixcan_file_pattern:    str,
+        smultixcan_file:            Path,
+        reference_panel:            RefPanel,
+        eqtl_model:                 EqtlModel,
+        output_dir:                 Path,
+        project_dir:                Path = conf.CURRENT_DIR,
+        ) -> Tuple[int, str]:
+    # @formatter:on
+    """
+    Invokes the gene-corr preprocess command with the given arguments.
+    """
+    _BASE_COMMAND = (
+        "run gene-corr preprocess "
+        "-c {cohort} "
+        "-r {reference_panel} "
+        "-m {eqtl_model} "
+        "-g {gwas_file} "
+        "-s {spredixcan_folder} "
+        "-n {spredixcan_file_pattern} "
+        "-f {smultixcan_file} "
+        "-p {project_dir} "
+        "-o {output_dir} "
+    )
+
+    # Build the command
+    command = _BASE_COMMAND.format(
+        cohort=cohort,
+        gwas_file=gwas_file,
+        spredixcan_folder=spredixcan_folder,
+        spredixcan_file_pattern=spredixcan_file_pattern,
+        smultixcan_file=smultixcan_file,
+        reference_panel=reference_panel,
+        eqtl_model=eqtl_model,
+        project_dir=project_dir,
+        output_dir=output_dir,
+    )
+    result = runner.invoke(cli.app, command)
+    success = result.exit_code == 0
+    message = result.stdout
+
+    gene_tissues_filename = "gene_tissues.pkl"
+    test_gene_tissues = output_dir / gene_tissues_filename
+    assert test_gene_tissues.exists(), f"gene-tissues.pkl not found in {output_dir}"
+
+    return success, message
+
+
+# @formatter:off
+def invoke_corr_correlate(
+        cohort:                         Cohort,
+        reference_panel:                RefPanel,
+        eqtl_model:                     EqtlModel,
+        chromosome:                     int,
+        smultixcan_condition_number:    int = 30,
+        compute_within_distance:        bool = False,
+        debug_mode:                     bool = False,
+        input_dir:                      Path = None,
+        output_dir:                     Path = None,
+        project_dir:                    Path = conf.CURRENT_DIR,
+) -> Tuple[int, str]:
+    # @formatter:on
+    """
+    Invokes the gene-corr correlate command with the given arguments.
+    """
+    _BASE_COMMAND = (
+        "run gene-corr correlate "
+        "-c {cohort} "
+        "-r {reference_panel} "
+        "-m {eqtl_model} "
+        "-s {chromosome} "
+        "-i {input_dir} "
+        "-p {project_dir} "
+        "-o {output_dir} "
+    )
+
+    # Build the command
+    command = _BASE_COMMAND.format(
+        cohort=cohort,
+        reference_panel=reference_panel,
+        eqtl_model=eqtl_model,
+        chromosome=chromosome,
+        smultixcan_condition_number=smultixcan_condition_number,
+        compute_within_distance=compute_within_distance,
+        debug_mode=debug_mode,
+        input_dir=input_dir,
+        output_dir=output_dir,
+        project_dir=project_dir,
+    )
+
+    # Execute the command using runner.invoke
+    result = runner.invoke(cli.app, command)
+    success = result.exit_code == 0
+    message = result.stdout
+    return success, message
+
+
+# @formatter:off
+def invoke_corr_postprocess(
+        cohort:                         Cohort,
+        reference_panel:                RefPanel,
+        eqtl_model:                     EqtlModel,
+        input_dir:                      Path = None,
+        genes_info:                     Path = None,
+        output_dir:                     Path = None,
+        project_dir:                    Path = conf.CURRENT_DIR,
+) -> Tuple[int, str]:
+    # @formatter:on
+    """
+    Invokes the gene-corr correlate command with the given arguments.
+    """
+    _BASE_COMMAND = (
+        "run gene-corr postprocess "
+        "-c {cohort} "
+        "-r {reference_panel} "
+        "-m {eqtl_model} "
+        "-i {input_dir} "
+        "-g {genes_info} "
+        "-o {output_dir} "
+    )
+
+    # Build the command
+    command = _BASE_COMMAND.format(
+        cohort=cohort,
+        reference_panel=reference_panel,
+        eqtl_model=eqtl_model,
+        input_dir=input_dir,
+        genes_info=genes_info,
+        output_dir=output_dir,
+        project_dir=project_dir,
+    )
+
+    # Execute the command using runner.invoke
+    result = runner.invoke(cli.app, command)
+    success = result.exit_code == 0
+    message = result.stdout
+    return success, message
+
+
+# @formatter:off
+def invoke_corr_filter(
+        cohort:                         Cohort,
+        reference_panel:                RefPanel,
+        eqtl_model:                     EqtlModel,
+        distances:                      List[float],
+        genes_symbols:                  Path,
+        output_dir:                     Path = None,
+        project_dir:                    Path = conf.CURRENT_DIR,
+) -> Tuple[int, str]:
+    # @formatter:on
+    """
+    Invokes the gene-corr correlate command with the given arguments.
+    """
+    _BASE_COMMAND = (
+        "run gene-corr postprocess "
+        "-c {cohort} "
+        "-r {reference_panel} "
+        "-m {eqtl_model} "
+        "-i {input_dir} "
+        "-g {genes_info} "
+        "-o {output_dir} "
+    )
+
+    # Build the command
+    command = _BASE_COMMAND.format(
+        cohort=cohort,
+        reference_panel=reference_panel,
+        eqtl_model=eqtl_model,
+        output_dir=output_dir,
+        project_dir=project_dir,
+    )
+
+    # Execute the command using runner.invoke
+    result = runner.invoke(cli.app, command)
+    success = result.exit_code == 0
+    message = result.stdout
+    return success, message
--- phenoplier/commands/run/correlation/pipeline.py
+++ phenoplier/commands/run/correlation/pipeline.py
@@ -1,6 +1,6 @@
 import logging
 import datetime
-from concurrent.futures import ProcessPoolExecutor, as_completed
+import concurrent.futures
 from typing import Annotated
 from pathlib import Path
 
@@ -14,8 +14,17 @@ from phenoplier.gls import GLSPhenoplier
 from phenoplier.commands.util.enums import Cohort, RefPanel, EqtlModel
 # from phenoplier.constants.cli import Corr_Generate_Args as Args
 from phenoplier.commands.util.utils import load_settings_files
-from phenoplier.constants.cli import Common_Args, Corr_Preprocess_Args, Corr_Pipeline_Args
-from phenoplier.commands.invoker import invoke_corr_preprocess
+from phenoplier.constants.cli import (
+    Common_Args,
+    Corr_Preprocess_Args,
+    Corr_Correlate_Args,
+    Corr_Pipeline_Args
+)
+
+from phenoplier.commands.invoker import (
+    invoke_corr_preprocess,
+    invoke_corr_correlate,
+    invoke_corr_postprocess)
 
 logger = logging.getLogger(__name__)
 
@@ -35,13 +44,21 @@ pipeline_res_dir = Path(conf.CURRENT_DIR) / "pipeline_results" / timestamp_str
 
 
 def pipeline(
-        cohort_name: Annotated[Cohort, Common_Args.COHORT_NAME.value],
+        # Common arguments
+        cohort: Annotated[Cohort, Common_Args.COHORT_NAME.value],
         reference_panel: Annotated[RefPanel, Common_Args.REFERENCE_PANEL.value],
         eqtl_model: Annotated[EqtlModel, Common_Args.EQTL_MODEL.value],
+        # Preprocess arguments
         gwas_file: Annotated[Path, Corr_Preprocess_Args.GWAS_FILE.value],
         spredixcan_folder: Annotated[Path, Corr_Preprocess_Args.SPREDIXCAN_FOLDER.value],
         spredixcan_file_pattern: Annotated[str, Corr_Preprocess_Args.SPREDIXCAN_FILE_PATTERN.value],
         smultixcan_file: Annotated[Path, Corr_Preprocess_Args.SMULTIXCAN_FILE.value],
+        # Correlate arguments
+        # chromosome: Annotated[int, Corr_Correlate_Args.CHROMOSOME.value] = None,
+        smultixcan_condition_number: Annotated[int, Corr_Correlate_Args.SMULTIXCAN_CONDITION_NUMBER.value] = 30,
+        compute_within_distance: Annotated[bool, Corr_Correlate_Args.COMPUTE_WITHIN_DISTANCE.value] = False,
+        correlate_debug_mode: Annotated[bool, Corr_Correlate_Args.DEBUG_MODE.value] = False,
+        # Common arguments with default values
         project_dir: Annotated[Path, Common_Args.PROJECT_DIR.value] = conf.CURRENT_DIR,
         output_dir: Annotated[Path, Corr_Pipeline_Args.OUTPUT_DIR.value] = pipeline_res_dir,
 ):
@@ -50,21 +67,80 @@ def pipeline(
     to be used in a cluster environment. Checkpoints will be created during the computation, so it can be resumed if
     interrupted. For finer-grained control, use the other commands below.
     """
-    logger.info("Running subroutine <preprocess>...")
-    suc, msg = invoke_corr_preprocess(cohort_name,
-                                      gwas_file,
-                                      spredixcan_folder,
-                                      spredixcan_file_pattern,
-                                      smultixcan_file,
-                                      reference_panel,
-                                      eqtl_model,
-                                      project_dir)
-    if suc:
-        logger.info("Subroutine <preprocess> finished successfully.")
-    else:
-        logger.error("Subroutine <preprocess> failed.")
-        logger.error(msg)
-        return
+    # logger.info("Running subroutine <preprocess>...")
+    # suc, msg = invoke_corr_preprocess(
+    #     cohort=cohort,
+    #     gwas_file=gwas_file,
+    #     spredixcan_folder=spredixcan_folder,
+    #     spredixcan_file_pattern=spredixcan_file_pattern,
+    #     smultixcan_file=smultixcan_file,
+    #     reference_panel=reference_panel,
+    #     eqtl_model=eqtl_model,
+    #     project_dir=project_dir,
+    #     output_dir=output_dir,
+    # )
+    # if suc:
+    #     logger.info("Subroutine <preprocess> finished successfully.")
+    # else:
+    #     logger.error("Subroutine <preprocess> failed.")
+    #     logger.error(msg)
+    #     exit(1)
+    #
+    # logger.info("Running subroutine <correlate>...")
+    logger.error(output_dir)
 
+    def run_correlate(chromosome):
+        suc, msg = invoke_corr_correlate(
+            cohort=cohort,
+            reference_panel=reference_panel,
+            eqtl_model=eqtl_model,
+            chromosome=chromosome,
+            smultixcan_condition_number=smultixcan_condition_number,
+            compute_within_distance=compute_within_distance,
+            debug_mode=correlate_debug_mode,
+            output_dir=output_dir,
+            project_dir=project_dir,
+        )
+        if suc:
+            logger.info(f"Subroutine <correlate> for chromosome {chromosome} finished successfully.")
+        else:
+            logger.error(f"Subroutine <correlate> for chromosome {chromosome} failed.")
+            logger.error(msg)
+            exit(1)
+        return suc
+
+    chromosomes = range(1, 2)
+    # Use ThreadPoolExecutor for multithreading
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        # Start the operations and mark each future with its chromosome
+        future_to_chromosome = {executor.submit(run_correlate, chromosome): chromosome for chromosome in chromosomes}
+
+        for future in concurrent.futures.as_completed(future_to_chromosome):
+            chromosome = future_to_chromosome[future]
+            try:
+                future.result()
+            except Exception as exc:
+                logger.error(f"Subroutine <correlate> for chromosome {chromosome} generated an exception: {exc}")
+                exit(1)
+    logger.info("Subroutine <correlate> finished successfully.")
+
+    # logger.info("Running subroutine <postprocess>...")
+    # postprocess_input_dir = output_dir / "by_chr"
+    # postprocess_genes_info_file = output_dir / "genes_info.pkl"
+    # suc, msg = invoke_corr_postprocess(
+    #     cohort=cohort,
+    #     reference_panel=reference_panel,
+    #     eqtl_model=eqtl_model,
+    #     input_dir=postprocess_input_dir,
+    #     genes_info=postprocess_genes_info_file,
+    #     output_dir=output_dir,
+    #     project_dir=project_dir,
+    # )
+    # if suc:
+    #     logger.info("Subroutine <postprocess> finished successfully.")
+    # else:
+    #     logger.error("Subroutine <postprocess> failed.")
+    #     logger.error(msg)
+    #     exit(1)
 
     return
