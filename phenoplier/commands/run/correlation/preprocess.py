f60a4ad4bf3953a2bb91e252a680612906609ffe
Haoyu Zhang <haoyu_z@outlook.com>
2024-08-13 15:19:39 -0600

    [corr/preprocess]: Add more runtime assertions

--- phenoplier/commands/run/correlation/preprocess.py
+++ phenoplier/commands/run/correlation/preprocess.py
@@ -17,15 +17,15 @@ from phenoplier.constants.cli import Corr_Preprocess_Args as Args
 
 # Todo: Validate reference_panel, check if folder exists. Or change it to a path argument?
 def preprocess(
-        cohort:                     Annotated[Cohort, Args.COHORT_NAME.value],
-        reference_panel:            Annotated[RefPanel, Args.REFERENCE_PANEL.value],
-        eqtl_model:                 Annotated[EqtlModel, Args.EQTL_MODEL.value],
-        gwas_file:                  Annotated[Path, Args.GWAS_FILE.value],
-        spredixcan_folder:          Annotated[Path, Args.SPREDIXCAN_FOLDER.value],
-        spredixcan_file_pattern:    Annotated[str, Args.SPREDIXCAN_FILE_PATTERN.value],
-        smultixcan_file:            Annotated[Path, Args.SMULTIXCAN_FILE.value],
-        project_dir:                Annotated[Path, Args.PROJECT_DIR.value] = conf.CURRENT_DIR,
-        output_dir:                 Annotated[Path, Args.OUTPUT_DIR.value] = None,
+        cohort: Annotated[Cohort, Args.COHORT_NAME.value],
+        reference_panel: Annotated[RefPanel, Args.REFERENCE_PANEL.value],
+        eqtl_model: Annotated[EqtlModel, Args.EQTL_MODEL.value],
+        gwas_file: Annotated[Path, Args.GWAS_FILE.value],
+        spredixcan_folder: Annotated[Path, Args.SPREDIXCAN_FOLDER.value],
+        spredixcan_file_pattern: Annotated[str, Args.SPREDIXCAN_FILE_PATTERN.value],
+        smultixcan_file: Annotated[Path, Args.SMULTIXCAN_FILE.value],
+        project_dir: Annotated[Path, Args.PROJECT_DIR.value] = conf.CURRENT_DIR,
+        output_dir: Annotated[Path, Args.OUTPUT_DIR.value] = None,
 ):
     """
     Compiles information about the GWAS and TWAS for a particular cohort. For example, the set of GWAS variants, variance of predicted expression of genes, etc.
@@ -101,15 +101,18 @@ def preprocess(
         gwas_file_path,
         sep="\t",
         usecols=["panel_variant_id", "pvalue", "zscore"],
-    ).dropna()
+    ).dropna()  # remove SNPs with no results
+    # Save GWAS variants
     gwas_variants_ids_set = frozenset(gwas_data["panel_variant_id"])
     with open(output_dir_base / "gwas_variant_ids.pkl", "wb") as handle:
         pickle.dump(gwas_variants_ids_set, handle, protocol=pickle.HIGHEST_PROTOCOL)
     print("Done.")
 
-    # Obtain tissue information
+    # TWAS data processing
+    # obtain tissue information
     prediction_model_tissues = get_model_tissue_names(eqtl_model)
     print(f"Loading smultixcan file: {smultixcan_file_path}")
+    # read S-MultiXcan results
     smultixcan_results = pd.read_csv(
         smultixcan_file_path, sep="\t", usecols=["gene", "gene_name", "pvalue", "n", "n_indep"]
     ).dropna().assign(gene_id=lambda x: x["gene"].apply(lambda g: g.split(".")[0]))
@@ -118,8 +121,8 @@ def preprocess(
     # Data Processing
     print(Text("[--- Data Processing ---]", style="blue"))
     with Progress(
-        SpinnerColumn(),
-        TextColumn("[progress.description]{task.description}"),
+            SpinnerColumn(),
+            TextColumn("[progress.description]{task.description}"),
     ) as progress:
         progress.add_task(description="Processing gene information...", total=None)
         # Process gene information
@@ -127,52 +130,107 @@ def preprocess(
         multiplier_gene_obj = {gene_name: Gene(name=gene_name) for gene_name in common_genes if
                                gene_name in Gene.GENE_NAME_TO_ID_MAP()}
 
+        assert multiplier_gene_obj["GAS6"].ensembl_id == "ENSG00000183087"
+
+        _gene_obj = list(multiplier_gene_obj.values())
         genes_info = pd.DataFrame({
-            "name": [g.name for g in multiplier_gene_obj.values()],
-            "id": [g.ensembl_id for g in multiplier_gene_obj.values()],
-            "chr": [g.chromosome for g in multiplier_gene_obj.values()],
-            "band": [g.band for g in multiplier_gene_obj.values()],
-            "start_position": [g.get_attribute("start_position") for g in multiplier_gene_obj.values()],
-            "end_position": [g.get_attribute("end_position") for g in multiplier_gene_obj.values()],
+            "name": [g.name for g in _gene_obj],
+            "id": [g.ensembl_id for g in _gene_obj],
+            "chr": [g.chromosome for g in _gene_obj],
+            "band": [g.band for g in _gene_obj],
+            "start_position": [g.get_attribute("start_position") for g in _gene_obj],
+            "end_position": [g.get_attribute("end_position") for g in _gene_obj],
         }).assign(gene_length=lambda x: x["end_position"] - x["start_position"]).dropna()
 
         genes_info["chr"] = genes_info["chr"].apply(pd.to_numeric, downcast="integer")
         genes_info["start_position"] = genes_info["start_position"].astype(int)
         genes_info["end_position"] = genes_info["end_position"].astype(int)
         genes_info["gene_length"] = genes_info["gene_length"].astype(int)
+
+        # check correctness
+        if not genes_info["name"].is_unique:
+            raise ValueError("genes_info has duplicate names.")
+        if not genes_info["id"].is_unique:
+            raise ValueError("genes_info has duplicate ids.")
+
+        # sort by chromosomes
+        genes_info.sort_values("chr")
+        # output results
         output_file = output_dir_base / "genes_info.pkl"
         genes_info.to_pickle(output_file)
-
-        spredixcan_result_files = {t: spredixcan_folder_path / spredixcan_file_pattern.format(tissue=t) for t in
-                                   prediction_model_tissues}
     print(f"Done. Gene information saved in: {output_file}")
 
     with Progress(
-        SpinnerColumn(),
-        TextColumn("[progress.description]{task.description}"),
+            SpinnerColumn(),
+            TextColumn("[progress.description]{task.description}"),
     ) as progress:
         progress.add_task(description="Loading S-PrediXcan results...", total=None)
         # Load S-PrediXcan results
+        spredixcan_result_files = {t: spredixcan_folder_path / spredixcan_file_pattern.format(tissue=t) for t in
+                                   prediction_model_tissues}
+        # check on input
+        if not len(spredixcan_result_files) == len(prediction_model_tissues):
+            raise ValueError("Length of spredixcan_result_files not equal to that of prediction_model_tissues.")
+
         spredixcan_dfs = pd.concat([
             pd.read_csv(f, usecols=["gene", "zscore", "pvalue", "n_snps_used", "n_snps_in_model"]).dropna(
                 subset=["gene", "zscore", "pvalue"]).assign(tissue=t)
             for t, f in spredixcan_result_files.items()
         ])
-        spredixcan_dfs = spredixcan_dfs.assign(gene_id=lambda x: x["gene"].apply(lambda g: g.split(".")[0]))
-        spredixcan_dfs = spredixcan_dfs[spredixcan_dfs["gene_id"].isin(set(genes_info["id"]))]
+        # check on dfs
+        if not len(spredixcan_dfs) == len(prediction_model_tissues):
+            raise ValueError()
 
+        spredixcan_dfs = spredixcan_dfs.assign(gene_id=lambda x: x["gene"].apply(lambda g: g.split(".")[0]))
+        # leave only common genes
+        spredixcan_dfs = spredixcan_dfs[spredixcan_dfs["gene_id"].isin(set(genes_info["id"]))]
         spredixcan_genes_n_models = spredixcan_dfs.groupby("gene_id")["tissue"].nunique()
+
+        # checks
+        _tmp_smultixcan_results_n_models = (
+            smultixcan_results.set_index("gene_id")["n"].astype(int).rename("tissue")
+        )
+        _cg = _tmp_smultixcan_results_n_models.index.intersection(
+            spredixcan_genes_n_models.index
+        )
+        _tmp_smultixcan_results_n_models = _tmp_smultixcan_results_n_models.loc[_cg]
+        _spredixcan = spredixcan_genes_n_models.loc[_cg]
+        if not _spredixcan.shape[0] == _tmp_smultixcan_results_n_models.shape[0]:
+            raise ValueError()
+        if not _spredixcan.equals(_tmp_smultixcan_results_n_models.loc[_spredixcan.index]):
+            raise ValueError()
+
+        # get tissues available per gene
         spredixcan_genes_models = spredixcan_dfs.groupby("gene_id")["tissue"].apply(lambda x: frozenset(x.tolist()))
+        # checks
+        if not spredixcan_genes_n_models.shape[0] == spredixcan_genes_models.shape[0]:
+            raise ValueError()
+        if not spredixcan_genes_n_models.index.equals(spredixcan_genes_models.index):
+            raise ValueError()
+        if not (spredixcan_genes_models.apply(len) <= len(prediction_model_tissues)).all():
+            raise ValueError()
+        if not (spredixcan_genes_models.loc[spredixcan_genes_n_models.index]
+                .apply(len).equals(spredixcan_genes_n_models)):
+            raise ValueError()
+
+        # Add gene name and set index
         spredixcan_genes_models = spredixcan_genes_models.to_frame().reset_index()
         spredixcan_genes_models = spredixcan_genes_models.assign(
             gene_name=spredixcan_genes_models["gene_id"].apply(lambda g: Gene.GENE_ID_TO_NAME_MAP()[g]))
         spredixcan_genes_models = spredixcan_genes_models[["gene_id", "gene_name", "tissue"]].set_index("gene_id")
+        # Add number of tissues
         spredixcan_genes_models = spredixcan_genes_models.assign(n_tissues=spredixcan_genes_models["tissue"].apply(len))
+        # check on output
+        if not spredixcan_genes_models["gene_name"].is_unique:
+            raise ValueError("spredixcan_genes_models has duplicate names.")
+        if spredixcan_genes_models.isna().any(None):
+            raise ValueError("spredixcan_genes_models has NaN values.")
+        # save output
         spredixcan_genes_models.to_pickle(output_dir_base / "gene_tissues.pkl")
 
         spredixcan_gene_obj = {gene_id: Gene(ensembl_id=gene_id) for gene_id in spredixcan_genes_models.index}
     print("Done")
-
+    # Todo: resume here
     def _get_gene_pc_variance(gene_row):
         gene_id = gene_row.name
         gene_tissues = gene_row["tissue"]
@@ -186,10 +244,12 @@ def preprocess(
         return s
 
     with Progress(
-        SpinnerColumn(),
-        TextColumn("[progress.description]{task.description}"),
+            SpinnerColumn(),
+            TextColumn("[progress.description]{task.description}"),
     ) as progress:
         progress.add_task(description="Computing gene PC variances (this step takes some time)...", total=None)
+        # Add covariates based on S-PrediXcan results
+
         spredixcan_genes_tissues_pc_variance = spredixcan_genes_models.apply(_get_gene_pc_variance, axis=1)
         spredixcan_genes_models = spredixcan_genes_models.join(
             spredixcan_genes_tissues_pc_variance.rename("tissues_pc_variances"))
@@ -212,23 +272,23 @@ def preprocess(
         return tissue_variances
 
 # Todo: check if this routine is necessary
-    # with Progress(
-    #     SpinnerColumn(),
-    #     TextColumn("[progress.description]{task.description}"),
-    # ) as progress:
-    #     progress.add_task(description="Computing gene variances (this step takes some time)...", total=None)
-    #     spredixcan_genes_tissues_variance = spredixcan_genes_models.apply(_get_gene_variances, axis=1)
-    #     spredixcan_genes_models = spredixcan_genes_models.join(
-    #         spredixcan_genes_tissues_variance.rename("tissues_variances"))
+# with Progress(
+#     SpinnerColumn(),
+#     TextColumn("[progress.description]{task.description}"),
+# ) as progress:
+#     progress.add_task(description="Computing gene variances (this step takes some time)...", total=None)
+#     spredixcan_genes_tissues_variance = spredixcan_genes_models.apply(_get_gene_variances, axis=1)
+#     spredixcan_genes_models = spredixcan_genes_models.join(
+#         spredixcan_genes_tissues_variance.rename("tissues_variances"))
 
-    #     spredixcan_genes_sum_of_n_snps_used = spredixcan_dfs.groupby("gene_id")["n_snps_used"].sum().rename(
-    #         "n_snps_used_sum")
-    #     spredixcan_genes_models = spredixcan_genes_models.join(spredixcan_genes_sum_of_n_snps_used)
+#     spredixcan_genes_sum_of_n_snps_used = spredixcan_dfs.groupby("gene_id")["n_snps_used"].sum().rename(
+#         "n_snps_used_sum")
+#     spredixcan_genes_models = spredixcan_genes_models.join(spredixcan_genes_sum_of_n_snps_used)
 
-    #     spredixcan_genes_sum_of_n_snps_in_model = spredixcan_dfs.groupby("gene_id")["n_snps_in_model"].sum().rename(
-    #         "n_snps_in_model_sum")
-    #     spredixcan_genes_models = spredixcan_genes_models.join(spredixcan_genes_sum_of_n_snps_in_model)
+#     spredixcan_genes_sum_of_n_snps_in_model = spredixcan_dfs.groupby("gene_id")["n_snps_in_model"].sum().rename(
+#         "n_snps_in_model_sum")
+#     spredixcan_genes_models = spredixcan_genes_models.join(spredixcan_genes_sum_of_n_snps_in_model)
 
-    #     output_file = output_dir_base / "spredixcan_genes_models.pkl"
-    #     spredixcan_genes_models.to_pickle(output_file)
-    # print(f"Done. Spreadixcan genes models saved in: {output_file}")
+#     output_file = output_dir_base / "spredixcan_genes_models.pkl"
+#     spredixcan_genes_models.to_pickle(output_file)
+# print(f"Done. Spreadixcan genes models saved in: {output_file}")
