2f650b7eac7a8c4089aa4332f29ee21461920ebc
Haoyu Zhang <haoyu_z@outlook.com>
2024-10-07 23:02:50 -0600

    [null/gwas]: Update script to launch multiple jobs for a specified phenotype range

--- nbs/null_simulations/05_gwas/90_00-gwas.sh
+++ nbs/null_simulations/05_gwas/90_00-gwas.sh
@@ -1,47 +1,88 @@
 #!/bin/bash
 source ../common/utils.sh
 
-start_pheno=700
-end_pheno=899
+# Parameters to specify the range and batch size
+start_pheno=0
+end_pheno=999
+batch_size=200
 
-job_name="PhenoPLIER Null Simulation GWAS"
+# Other constants
+job_name="\"PhenoPLIER Null Simulation GWAS\""
 pheno_file_name="random_phenotypes_df.phe"
 pheno_file="/phenoplier/null_sim/data/${pheno_file_name}"
-pheno_names=$(generate_pheno_names ${start_pheno} ${end_pheno})
 covar_names="sex,age,pc1,pc2,pc3,pc4,pc5,pc6,pc7,pc8,pc9,pc10"
 bgen_path="/Bulk/Imputation/Imputation from genotype (GEL)"
-bgen_name="ukb21008_c2_b0_v1.bgen"
-sample_path="/Bulk-DRL/GEL_imputed_sample_files_fixed" # Path to the fixed sample file
+sample_path="/Bulk-DRL/GEL_imputed_sample_files_fixed"
 data_field="ukb21008"
-output_dir="phenoplier/null_sim/output/${start_pheno}-${end_pheno}/"
 instance_type="mem1_ssd1_v2_x36"
 priority="low"
-tag="${start_pheno}-${end_pheno}"
+bgen_name="ukb21008_c2_b0_v1.bgen"
+output_dir_base="phenoplier/null_sim/output"
+tag_base=""
 
+# Function to split phenotypes into batches and run the GWAS command for each batch
+run_gwas_batch() {
+    local batch_start=$1
+    local batch_end=$2
 
-# Declare long options
-bgen_arg=""
-sample_arg=""
-extra_options="\""
-extra_options+="hide-covar --maf 0.01 --mac 50 --covar-name sex,age,pc1,pc2,pc3,pc4,pc5,pc6,pc7,pc8,pc9,pc10 "
-extra_options+="--pheno-name ${pheno_names}"
-extra_options+="\""
+    # Create a tag and output dir based on the batch
+    local output_dir="${output_dir_base}/${batch_start}-${batch_end}/"
+    local tag="${batch_start}-${batch_end}"
 
-# Run on 22 chromosomes
-for i in {1..22}; do
-    bgen_arg+="-igenotypes_bgens=\"${bgen_path}/${data_field}_c${i}_b0_v1.bgen\" "
-    sample_arg+="-isample_ids_sample=\"${sample_path}/${data_field}_c${i}_b0_v1.sample\" "
+    # Generate phenotype names for this batch
+    pheno_names=$(generate_pheno_names ${batch_start} ${batch_end})
+
+    # Declare long options for the command
+    bgen_arg=""
+    sample_arg=""
+    extra_options="\""
+    extra_options+="hide-covar --maf 0.01 --mac 50 --covar-name ${covar_names} "
+    extra_options+="--pheno-name ${pheno_names}"
+    extra_options+="\""
+
+    # Run on 22 chromosomes
+    for i in {1..22}; do
+        bgen_arg+="-igenotypes_bgens=\"${bgen_path}/${data_field}_c${i}_b0_v1.bgen\" "
+        sample_arg+="-isample_ids_sample=\"${sample_path}/${data_field}_c${i}_b0_v1.sample\" "
+    done
+
+    # Build the command
+    command="dx run plink_gwas \
+        --name ${job_name} \
+        --instance-type ${instance_type} \
+        --tag ${tag} \
+        --priority ${priority} \
+        --destination=${output_dir} \
+        ${bgen_arg} ${sample_arg} \
+        -iphenotypes_pheno=${pheno_file} \
+        -icovariates_cov=${pheno_file} \
+        -iextra_options=${extra_options}"
+
+    # Print the command for debugging
+    echo "Running GWAS for phenotypes ${batch_start} to ${batch_end}:"
+    echo "${command}"
+
+    # Run the command
+    eval "${command}"
+}
+
+# Main loop to split the phenotypes into batches and run the GWAS command for each batch
+# For example:
+# With start_pheno=0, end_pheno=999, and batch_size=200, the script will create 5 batches:
+# Batch 1: Phenotypes 0 to 199
+# Batch 2: Phenotypes 200 to 399
+# Batch 3: Phenotypes 400 to 599
+# Batch 4: Phenotypes 600 to 799
+# Batch 5: Phenotypes 800 to 999
+
+for ((batch_start=${start_pheno}; batch_start<=${end_pheno}; batch_start+=${batch_size})); do
+    batch_end=$((batch_start + batch_size - 1))
+
+    # Ensure batch_end does not exceed end_pheno
+    if [[ ${batch_end} -gt ${end_pheno} ]]; then
+        batch_end=${end_pheno}
+    fi
+
+    # Call the function to run GWAS for this batch
+    run_gwas_batch ${batch_start} ${batch_end}
 done
-
-command="dx run 
-    --name ${job_name} \
-    --instance-type ${instance_type} \
-    --tag ${tag} \
-    --priority ${priority} \
-    --destination=${output_dir}
-    plink_gwas ${bgen_arg} ${sample_arg} \
-    -iphenotypes_pheno=${pheno_file} \
-    -icovariates_cov=${pheno_file} \
-    -iextra_options=${extra_options}"
-echo $command
-eval $command
