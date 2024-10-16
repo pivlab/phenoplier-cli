659b423de7e6d74d13bb987108409826a7caf81c
Haoyu Zhang <haoyu_z@outlook.com>
2024-10-16 11:08:12 -0600

    [nullsim]: Save current scripts

---
+++ nbs/null_simulations/common/rap.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+run_cloud_workstation() {
+  local max_session_length="$1"
+  local session_name="$2"
+  local tag="$3"
+  local instance_type="$4"
+
+  dx run app-cloud_workstation \
+    -imax_session_length="${max_session_length}" \
+    --brief -y \
+    --name "${session_name}" \
+    --tag "${tag}" \
+    --instance-type "${instance_type}" \
+    --priority "low"
+}
