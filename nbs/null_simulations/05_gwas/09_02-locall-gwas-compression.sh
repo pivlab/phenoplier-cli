c9e420d32a46bf26fd004f1cdde63e3e87fdb92d
Haoyu Zhang <haoyu_z@outlook.com>
2024-11-23 15:36:30 -0700

    [null/gwas]: Update script for generating 10 random phenotypes

---
+++ nbs/null_simulations/05_gwas/09_02-locall-gwas-compression.sh
@@ -0,0 +1,10 @@
+# Use this script if you plan to compress the GWAS results locally
+
+source ../common/utils.sh
+
+data_dpath="/mnt/data/proj_data/nullsim/raw"
+start_pheno=0
+end_pheno=9
+N_JOBS=$(nproc --all)
+
+parallel -j${N_JOBS} compress_gwas {} ~/output "add-phenoid-padding" ::: ${data_dpath}/${start_pheno}-${end_pheno}/*.assoc.txt
