3ec97444507ea391314f63457648865a43e1bae1
Haoyu Zhang <haoyu_z@outlook.com>
2024-12-02 22:52:00 -0700

    [doc]: Update configuration

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,18 +10,34 @@ project = 'PhenoPLIER'
 copyright = '2024, Milton Pividori, Haoyu Zhang'
 author = 'Milton Pividori, Haoyu Zhang'
 
+release = '0.1'
+version = '0.1.0'
+
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = []
 
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+]
 
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'pydata_sphinx_theme'
 html_static_path = ['_static']
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
