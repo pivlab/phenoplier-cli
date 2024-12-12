f9353d2e9b22289e032e4de8644755b238d1c637
Haoyu Zhang <haoyu_z@outlook.com>
2024-12-11 18:00:13 -0700

    [cli/proj]: Add cli interface for data projection

--- phenoplier/commands/project.py
+++ phenoplier/commands/project.py
@@ -1,21 +1,36 @@
 """
 This module is a sub-command of the main CLI. It projects input data into the specified representation space.
 """
+import phenoplier.multiplier as multiplier
 
-from enum import Enum
 from pathlib import Path
 from typing import Annotated
-import typer
-from phenoplier.data import Downloader
+
 from phenoplier.commands.util.utils import load_settings_files
 from phenoplier.constants.arg import Common_Args
+from phenoplier.constants.arg import Project_Args as args
 from phenoplier.config import settings as conf
+from phenoplier.utils import read_rds
 
 
-def multiplier(
+def to_multiplier(
+    input_file:  Annotated[Path, args.INPUT_FILE.value],
+    output_file: Annotated[Path, args.OUTPUT_FILE.value] = None,
     project_dir: Annotated[Path, Common_Args.PROJECT_DIR.value] = conf.CURRENT_DIR
 ):
     """
     Projects new data into the MultiPLIER latent space.
     """
     load_settings_files(project_dir)
+
+    if not input_file.exists():
+        raise FileNotFoundError(f"Input file not found: {input_file}")
+    
+    # Generate default output filename if not provided
+    if output_file is None:
+        output_file = input_file.parent / f"{input_file.stem}_projected_to_m.pkl"
+    
+    rds_data = read_rds(input_file)
+    proj_data = multiplier.transform(rds_data)
+    # save the projected data as pd.DataFrame
+    proj_data.to_pickle(output_file)
