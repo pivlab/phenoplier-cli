461536eac4d5ca664c8c14759b35ba178eaab230
Haoyu Zhang <haoyu_z@outlook.com>
2024-12-18 17:01:51 -0700

    [build]: Bump up test version to 0.2.7

--- phenoplier/__init__.py
+++ phenoplier/__init__.py
@@ -1,2 +1,2 @@
 __name__ = "phenoplier"
-__version__ = "0.2.6" # Remember to update this version number in project.toml as well
+__version__ = "0.2.7" # Remember to update this version number in project.toml as well
