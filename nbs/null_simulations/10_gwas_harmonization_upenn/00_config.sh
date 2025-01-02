c02c61150342de533ef664c344b3255245312ea5
Haoyu Zhang <haoyu_z@outlook.com>
2025-01-02 15:30:42 -0700

    [twas]: Update config.sh

--- nbs/null_simulations/10_gwas_harmonization_upenn/00_config.sh
+++ nbs/null_simulations/10_gwas_harmonization_upenn/00_config.sh
@@ -1,1 +1,7 @@
-PHENOPLIER_GWAS_IMPUTATION_BASE_DIR="/tmp/phenoplier/summary-gwas-imputation/"
+# This scirpt initilizes the environment for each cluster job
+
+# Use Alpine's scratch directory for all compute jobs
+# Caution: files are automatically removed 90 days after their initial creation in this directory.
+export PHENOPLIER_ROOT_DIR="/scratch/alpine/${USER}/phenoplier"
+export PHENOPLIER_GWAS_IMPUTATION_BASE_DIR="${PHENOPLIER_ROOT_DIR}/summary_gwas_imputation/"
+
