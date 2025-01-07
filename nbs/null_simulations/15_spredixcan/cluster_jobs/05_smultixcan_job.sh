45813dbee95ef7bac5fa037a974f315b72e32ba9
Haoyu Zhang <haoyu_z@outlook.com>
2025-01-07 00:06:53 -0700

    [null/twas]: Adapat and execuet spredixcan scripts

--- nbs/null_simulations/15_spredixcan/cluster_jobs/05_smultixcan_job.sh
+++ nbs/null_simulations/15_spredixcan/cluster_jobs/05_smultixcan_job.sh
@@ -11,7 +11,7 @@
 # BSUB -W 1:00
 
 # make sure we use the number of CPUs specified
-export n_jobs=1
+export n_jobs=16
 export PHENOPLIER_N_JOBS=${n_jobs}
 export NUMBA_NUM_THREADS=${n_jobs}
 export MKL_NUM_THREADS=${n_jobs}
@@ -19,12 +19,12 @@ export OPEN_BLAS_NUM_THREADS=${n_jobs}
 export NUMEXPR_NUM_THREADS=${n_jobs}
 export OMP_NUM_THREADS=${n_jobs}
 
-CODE_DIR=${PHENOPLIER_CODE_DIR}/nbs/15_gsa_gls/20-null_simulations/15_spredixcan
-FINAL_IMPUTED_GWAS_DIR="${PHENOPLIER_RESULTS_GLS_NULL_SIMS}/final_imputed_gwas"
-SPREDIXCAN_DIR="${PHENOPLIER_RESULTS_GLS_NULL_SIMS}/twas/spredixcan"
-OUTPUT_DIR="${PHENOPLIER_RESULTS_GLS_NULL_SIMS}/twas/smultixcan"
+CODE_DIR=${PHENOPLIER_REPO_DIR}/nbs/null_simulations/15_spredixcan
+FINAL_IMPUTED_GWAS_DIR="${PHENOPLIER_RESULTS_GLS_NULL_SIMS_UKB_50PCS}/final_imputed_gwas"
+SPREDIXCAN_DIR="${PHENOPLIER_RESULTS_GLS_NULL_SIMS_UKB_50PCS}/twas/spredixcan"
+OUTPUT_DIR="${PHENOPLIER_RESULTS_GLS_NULL_SIMS_UKB_50PCS}/twas/smultixcan"
 
-GWAS_JOBINDEX=`expr ${LSB_JOBINDEX} - 1`
+# GWAS_JOBINDEX=`expr ${LSB_JOBINDEX} - 1`
 
 # +
 bash ${CODE_DIR}/05_smultixcan.sh \
