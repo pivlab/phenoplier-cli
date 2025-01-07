065443b865ccf3419ae79fa864f1431e7f53d29a
Haoyu Zhang <haoyu_z@outlook.com>
2025-01-06 22:06:31 -0700

    [null/twas]: Compute common variants

--- nbs/null_simulations/10_gwas_harmonization_desktop/cluster_jobs/15-common_variant_ids_job.sh
+++ nbs/null_simulations/10_gwas_harmonization_desktop/cluster_jobs/15-common_variant_ids_job.sh
@@ -19,13 +19,13 @@ export OPEN_BLAS_NUM_THREADS=${n_jobs}
 export NUMEXPR_NUM_THREADS=${n_jobs}
 export OMP_NUM_THREADS=${n_jobs}
 
-CODE_DIR=${PHENOPLIER_CODE_DIR}/nbs/null_simulations/10_gwas_harmonization_desktop
+CODE_DIR=${PHENOPLIER_REPO_DIR}/nbs/null_simulations/10_gwas_harmonization_desktop
 POST_GWAS_DIR="${PHENOPLIER_RESULTS_GLS_NULL_SIMS_UKB_50PCS}/post_imputed_gwas"
 OUTPUT_DIR="${PHENOPLIER_RESULTS_GLS_NULL_SIMS_UKB_50PCS}/final_imputed_gwas"
 mkdir -p ${OUTPUT_DIR}
 
 # GWAS_JOBINDEX=`expr $LSB_JOBINDEX - 1`
-GWAS_JOBINDEX="0000"
+# GWAS_JOBINDEX="0000"
 
 python ${CODE_DIR}/15_common_variant_ids.py \
   --input-gwas-file ${POST_GWAS_DIR}/random.pheno${GWAS_JOBINDEX}.glm-imputed.txt.gz \
