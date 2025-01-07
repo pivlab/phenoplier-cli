f7dd4a4a301416f4d3610c97fe17cc864e6d3de8
Haoyu Zhang <haoyu_z@outlook.com>
2025-01-06 20:43:41 -0700

    [null/twas]: Add driver script for common variant alignment

--- nbs/null_simulations/10_gwas_harmonization_desktop/cluster_jobs/15-common_variant_ids_job.sh
+++ nbs/null_simulations/10_gwas_harmonization_desktop/cluster_jobs/15-common_variant_ids_job.sh
@@ -11,7 +11,7 @@
 #BSUB -W 0:30
 
 # make sure we use the number of CPUs specified
-export n_jobs=1
+export n_jobs=16
 export PHENOPLIER_N_JOBS=${n_jobs}
 export NUMBA_NUM_THREADS=${n_jobs}
 export MKL_NUM_THREADS=${n_jobs}
@@ -19,12 +19,13 @@ export OPEN_BLAS_NUM_THREADS=${n_jobs}
 export NUMEXPR_NUM_THREADS=${n_jobs}
 export OMP_NUM_THREADS=${n_jobs}
 
-CODE_DIR=${PHENOPLIER_CODE_DIR}/nbs/15_gsa_gls/20-null_simulations/10_gwas_harmonization
-POST_GWAS_DIR="${PHENOPLIER_RESULTS_GLS_NULL_SIMS}/post_imputed_gwas"
-OUTPUT_DIR="${PHENOPLIER_RESULTS_GLS_NULL_SIMS}/final_imputed_gwas"
+CODE_DIR=${PHENOPLIER_CODE_DIR}/nbs/null_simulations/10_gwas_harmonization_desktop
+POST_GWAS_DIR="${PHENOPLIER_RESULTS_GLS_NULL_SIMS_UKB_50PCS}/post_imputed_gwas"
+OUTPUT_DIR="${PHENOPLIER_RESULTS_GLS_NULL_SIMS_UKB_50PCS}/final_imputed_gwas"
 mkdir -p ${OUTPUT_DIR}
 
-GWAS_JOBINDEX=`expr $LSB_JOBINDEX - 1`
+# GWAS_JOBINDEX=`expr $LSB_JOBINDEX - 1`
+GWAS_JOBINDEX="0000"
 
 python ${CODE_DIR}/15_common_variant_ids.py \
   --input-gwas-file ${POST_GWAS_DIR}/random.pheno${GWAS_JOBINDEX}.glm-imputed.txt.gz \
